29e9ef183bfc85a266a7e62f2143eed07cf410bc
yuang <13526739259@163.com>
2019-11-01 16:57:59 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Icon from "./user-active.png"
 import Data from './data.xml';
 function component() {
     var element = document.createElement('div');
-
+// 陈国强沙漠之鹰
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
